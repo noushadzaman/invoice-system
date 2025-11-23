ceb39bafd3e10ae83e7009bfab52b31799dad491
Noushad Zaman <96263586+noushadzaman@users.noreply.github.com>
2025-11-23 21:07:07 +0600

    auth with clerk

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,68 +14,94 @@ import { CirclePlus } from "lucide-react";
 import Link from "next/link";
 import { db } from "@/db";
 import { Invoices } from "@/db/schema";
+import Container from "@/components/Container";
+import { cn } from "@/lib/utils";
 
 export default async function Home() {
-  const results = await db.select().from(Invoices);
+  const invoices = await db.select().from(Invoices);
 
   return (
-    <div className="flex flex-col justify-center h-full text-center mx-auto max-w-5xl">
-      <div className="flex justify-between my-12">
-        <h1 className="text-3xl font-semibold">Invoices</h1>
-        <p>
-          <Button className="" variant="ghost" asChild>
-            <Link href="/invoices/new">
-              <CirclePlus className="h-4 w-4" />
-              Create invoice
-            </Link>
-          </Button>
+    <main className="h-full">
+      <Container>
+        <p className="bg-yellow-100 text-sm text-yellow-800 text-center px-3 py-2 rounded-lg mb-6">
+          Displaying all invoices for public demo. Creation is disabled.
         </p>
-      </div>
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px] p-4">Date</TableHead>
-            <TableHead className="p-4">Customer</TableHead>
-            <TableHead className="p-4">Email</TableHead>
-            <TableHead className="text-center p-4">Status</TableHead>
-            <TableHead className="text-right p-4">Value</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {results.map((result) => {
-            return (
-              <TableRow key={result.id}>
-                <TableCell className="font-medium text-left p-0">
-                  <Link href={`/invoices/${result.id}`} className="p-4 block">
-                    {new Date(result.createTs).toLocaleDateString()}
-                  </Link>
-                </TableCell>
-                <TableCell className="text-left p-0">
-                  <Link className="p-4 block" href={`/invoices/${result.id}`}>
-                    {result.name}
-                  </Link>
-                </TableCell>
-                <TableCell className="text-left p-0">
-                  <Link className="p-4 block" href={`/invoices/${result.id}`}>
-                    {result.email}
-                  </Link>
-                </TableCell>
-                <TableCell className="text-center p-0">
-                  <Link className="p-4 block" href={`/invoices/${result.id}`}>
-                    <Badge>{result.status}</Badge>
-                  </Link>
-                </TableCell>
-                <TableCell className="text-right p-0">
-                  <Link className="p-4 block" href={`/invoices/${result.id}`}>
-                    ${(result.value / 100).toFixed(2)}
-                  </Link>
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </div>
+        <div className="flex justify-between mb-6">
+          <h1 className="text-3xl font-semibold">Invoices</h1>
+          <p>
+            <Button className="inline-flex gap-2" variant="ghost" asChild>
+              <Link href="/invoices/new">
+                <CirclePlus className="h-4 w-4" />
+                Create Invoice
+              </Link>
+            </Button>
+          </p>
+        </div>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px] p-4">Date</TableHead>
+              <TableHead className="p-4">Customer</TableHead>
+              <TableHead className="p-4">Email</TableHead>
+              <TableHead className="text-center p-4">Status</TableHead>
+              <TableHead className="text-right p-4">Value</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((result) => {
+              return (
+                <TableRow key={result.id}>
+                  <TableCell className="font-medium text-left p-0">
+                    <Link
+                      href={`/invoices/${result.id}`}
+                      className="block p-4 font-semibold"
+                    >
+                      {new Date(result.createTs).toLocaleDateString()}
+                    </Link>
+                  </TableCell>
+                  <TableCell className="text-left p-0">
+                    <Link
+                      href={`/invoices/${result.id}`}
+                      className="block p-4 font-semibold"
+                    >
+                      {result.name}
+                    </Link>
+                  </TableCell>
+                  <TableCell className="text-left p-0">
+                    <Link className="block p-4" href={`/invoices/${result.id}`}>
+                      {result.email}
+                    </Link>
+                  </TableCell>
+                  <TableCell className="text-center p-0">
+                    <Link className="block p-4" href={`/invoices/${result.id}`}>
+                      <Badge
+                        className={cn(
+                          "rounded-full capitalize",
+                          result.status === "open" && "bg-blue-500",
+                          result.status === "paid" && "bg-green-600",
+                          result.status === "void" && "bg-zinc-700",
+                          result.status === "uncollectible" && "bg-red-600"
+                        )}
+                      >
+                        {result.status}
+                      </Badge>
+                    </Link>
+                  </TableCell>
+                  <TableCell className="text-right p-0">
+                    <Link
+                      href={`/invoices/${result.id}`}
+                      className="block p-4 font-semibold"
+                    >
+                      ${(result.value / 100).toFixed(2)}
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Container>
+    </main>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center h-full text-center mx-auto max-w-5xl">
+    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">Invoicipedia</h1>
       <p>
-        <Button>
+        <Button asChild>
           <Link href="/dashboard">Sign In</Link>
         </Button>
       </p>
-    </div>
+    </main>
   );
 }
